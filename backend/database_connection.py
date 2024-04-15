5454cf318031835d93f97ba5a2cd5e36d5cb6342
Nilson De la ossa <92598175+NilsOnfire@users.noreply.github.com>
2024-04-15 09:15:17 -0500

    Update database_connection.py ssl

--- backend/database_connection.py
+++ backend/database_connection.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 def get_collection(model_name):
     uri = "mongodb+srv://delaossa777:Aloha123%2A@dbindra.wplghu5.mongodb.net/?retryWrites=true&w=majority&appName=dbIndra"
 
-    client = AsyncIOMotorClient(uri)
+    client = AsyncIOMotorClient(uri,ssl=False)
     database = client.userDatabase
-    collection = database[model_name,ssl=False]
+    collection = database[model_name]
     return collection
